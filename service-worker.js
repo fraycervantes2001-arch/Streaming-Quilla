9b1feb5fa5eb988ce6e756d478759d5f2e500d57
fraycervantes2001-arch <fraycervantes2001@gmail.com>
2026-03-10 19:54:56 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,42 @@
-const CACHE_NAME = "streaming-quilla-v1";
-
+const CACHE_NAME = 'quilla-cache-v1';
 const urlsToCache = [
-"/",
-"/index.html",
-"/style.css",
-"/quilla.js"
+  '/',
+  '/index.html',
+  '/style.css',
+  '/quilla.js',
+  '/particles.js',
+  '/logo de streaming Quilla.jpeg',
+  '/favicon.ico'
 ];
 
-self.addEventListener("install", event => {
-
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
-
-);
-
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Archivos cacheados');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
-event.respondWith(
-
-caches.match(event.request)
-.then(response => response || fetch(event.request))
-
-);
-
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
