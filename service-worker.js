6b29b017876041f9dddd4950f3d05a677e489aa5
fraycervantes2001-arch <fraycervantes2001@gmail.com>
2026-03-10 21:35:20 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,42 @@
-const CACHE_NAME = 'quilla-cache-v1';
+const CACHE_NAME = "quilla-cache-v2";
+
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/quilla.js',
-  '/particles.js',
-  '/logo de streaming Quilla.jpeg',
-  '/favicon.ico'
+  "./",
+  "./index.html",
+  "./style.css",
+  "./quilla.js",
+  "./particles.js",
+  "./manifest.json",
+  "./logo de streaming Quilla.jpeg",
+  "./logo de netflix.png",
+  "./logo de disney+.png",
+  "./logo de hbomax.png",
+  "./logo de prime video.png",
+  "./logo de crunchyroll.png",
+  "./logo Paramount.png",
+  "./youtube.png",
+  "./Apple_TV+_logo.png",
+  "./vix.png",
+  "./plex.png",
+  "./logo chatgpt.png",
+  "./canva.png",
+  "./logo de deezer.png",
+  "./picsart.png",
+  "./logo de spotify.png",
+  "./win play.png",
+  "./icons8-whatsapp-50.png"
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Archivos cacheados');
+      console.log("Archivos cacheados");
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
@@ -26,7 +44,7 @@ self.addEventListener('fetch', (event) => {
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
